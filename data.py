480fa3bdc261ab78e7c192de605c7bc92051aceb
TIERESAID <vendeurn1@gmail.com>
2021-10-14 09:19:49 +0200

    front_end

---
+++ data.py
@@ -0,0 +1,35 @@
+def Articles():
+    articles = [
+        {
+            "id" : 1,
+            "title": "Article One",
+            "body" : "s eveniet architecto neque praesentium. Non Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe, deserunt. Molestiae, error consequuntur. Voluptatibus libero tenetur odit quam quibusdam nam provident explicabo similique commodi illo labore rerum, voluptates quod facilis unde tempora nostrum consectetur. Dolorem quod deleniti voluptatem, doloribus tempora aliquid qui accusantium fugit mollitia deserunt molestiae facere reprehenderit exercitationem nihil esse non culpa rerum fuga est consequuntur quibusdam itaque voluptatum nobis distinctio. Voluptatibus doloremque impedit repellat libero delectus natus voluptas ratione consequuntur voluptate! Architecto dolor soluta",
+            "author" : "Tiere Stephane",
+            "create_date" : "14-10-2021"
+        },
+
+        {
+            "id" : 2,
+            "title": "Article two",
+            "body" : "s eveniet architecto neque praesentium. Non Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe, deserunt. Molestiae, error consequuntur. Voluptatibus libero tenetur odit quam quibusdam nam provident explicabo similique commodi illo labore rerum, voluptates quod facilis unde tempora nostrum consectetur. Dolorem quod deleniti voluptatem, doloribus tempora aliquid qui accusantium fugit mollitia deserunt molestiae facere reprehenderit exercitationem nihil esse non culpa rerum fuga est consequuntur quibusdam itaque voluptatum nobis distinctio. Voluptatibus doloremque impedit repellat libero delectus natus voluptas ratione consequuntur voluptate! Architecto dolor soluta",
+            "author" : "Tibag",
+            "create_date" : "04-10-2021"
+        },
+
+        {
+            "id" : 3,
+            "title": "Article Three",
+            "body" : "s eveniet architecto neque praesentium. Non Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe, deserunt. Molestiae, error consequuntur. Voluptatibus libero tenetur odit quam quibusdam nam provident explicabo similique commodi illo labore rerum, voluptates quod facilis unde tempora nostrum consectetur. Dolorem quod deleniti voluptatem, doloribus tempora aliquid qui accusantium fugit mollitia deserunt molestiae facere reprehenderit exercitationem nihil esse non culpa rerum fuga est consequuntur quibusdam itaque voluptatum nobis distinctio. Voluptatibus doloremque impedit repellat libero delectus natus voluptas ratione consequuntur voluptate! Architecto dolor soluta",
+            "author" : "Ange Dour",
+            "create_date" : "09-10-2021"
+        },
+
+        {
+            "id" : 4,
+            "title": "Article Four",
+            "body" : "s eveniet architecto neque praesentium. Non Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe, deserunt. Molestiae, error consequuntur. Voluptatibus libero tenetur odit quam quibusdam nam provident explicabo similique commodi illo labore rerum, voluptates quod facilis unde tempora nostrum consectetur. Dolorem quod deleniti voluptatem, doloribus tempora aliquid qui accusantium fugit mollitia deserunt molestiae facere reprehenderit exercitationem nihil esse non culpa rerum fuga est consequuntur quibusdam itaque voluptatum nobis distinctio. Voluptatibus doloremque impedit repellat libero delectus natus voluptas ratione consequuntur voluptate! Architecto dolor soluta",
+            "author" : "Kante Bov",
+            "create_date" : "14-09-2020"
+        }
+    ]
+    return articles
