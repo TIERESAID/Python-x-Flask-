667509be7abfe0c6c28e72dc321510fa665415ea
TIERESAID <vendeurn1@gmail.com>
2021-10-22 09:04:15 +0200

    adding register field

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,flash,redirect,url_for,session,logging,request
 from data import Articles
+import sqlite3
+from flask import g
+from wtforms import Form , StringField, TextAreaField, PasswordField , validators
+from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
+
+DATABASE = "flaskapp.db"
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
 
 Articles = Articles()
 
@@ -21,6 +40,44 @@ def articles():
 def article(id):
     return render_template("article.html", id = id)
 
+class Registerform(Form):
+    name = StringField( "Name", [validators.Length(min=1, max=50)] )
+    username = StringField( "Username",[validators.Length(min=4 , max=50)] )
+    email = StringField("Email",[validators.Length(min=6, max=50)])
+    password = PasswordField(
+        "Password", 
+        [validators.data_required(),
+        validators.EqualTo("confirm", message = "Password do not match")
+        ]
+    )
+    confirm = PasswordField("Confirm Password")
+    
+@app.route("/register", methods=["GET","POST"])
+def register():
+    form = Registerform(request.form)
+    if request.method == "POST" and form.validate() :
+        try:
+
+            name = form.name.data
+            email = form.email.data
+            username = form.username.data
+            password = sha256_crypt.encrypt(str(form.password.data))
+
+            # create cursor 
+            db = get_db()
+            cur = db.cursor()
+            cur.execute( "INSERT INTO users(name , email,username,password) VALUES (?,?,?,?)",(name, email,username,password) )
+            
+            db.commit()
+            flash("Registered succes , you can now logging", "success")
+        except:
+            db.rollback()
+            flash("error in insertion operation", "error")
+        finally:
+            db.close()
+            redirect(url_for("loggin"))
+    return render_template("register.html", form = form)
 
 if __name__ == "__main__":
+    app.secret_key = "1234"
     app.run(debug=True)
